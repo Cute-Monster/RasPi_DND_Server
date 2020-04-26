1b315db043c9ba69e7af09e5919088f7cf03ceb7
cutemonsterhome <ghostshow@yandex.ru>
2020-04-26 20:22:20 +0300

    Add Logging.py, access.log
    Signed-off-by: Constantin <ghostshow@yandex.ru>

--- src/Classes/Logging.py
+++ src/Classes/Logging.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import os
 
+
 class Log:
     def __init__(self, module):
         self.error_log_file = open("{}/errors.log".format(os.path.abspath('logs')), "a")
@@ -9,19 +10,20 @@ class Log:
         self.module = module
 
         self.priority = {
-            1:"CRITICAL",
-            2:"WARNING",
-            3:"INFO",
-            4:"DEBUG"
+            1: "CRITICAL",
+            2: "WARNING",
+            3: "INFO",
+            4: "DEBUG"
         }
 
     def log_all(self, priority, string):
-        (self.access_log_file if priority == 3 or priority == 4 else self.error_log_file).writelines("{}: {}: {}: {}\n".format(
-            datetime.now(),
-            self.module,
-            self.priority.get(priority),
-            string
-        ))
+        (self.access_log_file if priority == 3 or priority == 4 else self.error_log_file).writelines(
+            "{}: {}: {}: {}\n".format(
+                datetime.now(),
+                self.module,
+                self.priority.get(priority),
+                string
+            ))
 
     def log_close(self):
         self.error_log_file.close()
