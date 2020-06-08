cef3173bb76ecbfb0b821177040c16eb783ee3cd
cutemonsterhome <ghostshow@yandex.ru>
2020-06-08 19:15:05 +0300

    add launch sh script
    Signed-off-by: cutemonsterhome <ghostshow@yandex.ru>

---
+++ Launch/start_server.sh
@@ -0,0 +1,3 @@
+echo "Started at : $( date) and $(/home/pi/Desktop/DND_Server/venv/bin/python3.7 --version)" >> /home/pi/Desktop/DND_Server/Logs/terminal.log
+cd /home/pi/Desktop/DND_Server
+/home/pi/Desktop/DND_Server/venv/bin/python3.7 server.py
